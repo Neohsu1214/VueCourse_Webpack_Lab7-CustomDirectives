4196162f835c0f3519e19c9d11d2405301c7da51
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-07 23:34:52 +0800

    建立一個客製化的 v-directive．此例為 v-highlight

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
                 <p v-text="'Some Text'"></p>
                 <p v-html="'<strong>Some strong text</strong>'"></p>
             </div>
+            <hr>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <!-- 以下會說明如何建立『客製化』的 v-directive -->
+                <!-- 此處在提醒一下，v-提示="" 裡面的東西都是 javascript，所以若只是要顯示string，記得要加上'' -->
+                <p v-highlight="'red'">Ｃolor this with v-highlight!</p>
+            </div>
         </div>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 建立『客製化』的 v-提示，此例建立的是 v-highlight
+Vue.directive('highlight', {
+  // v-directive有五個lifecycle hook: bind, inserted, update, componentUpdated, unbind
+  /**
+   * Directive hook 1: bind
+   * @param {*} el element
+   * @param {*} binding binding就是 v-directive="這裡所給予的值叫做binding"
+   * @param {*} vnode virtual node
+   */
+  bind: function(el, binding, vnode) { // 也可寫成 bind() {}，以下同理
+    // 將 backgroundColor 固定設為 green
+    // el.style.backgroundColor = 'green';
+
+    // 以下說明如何透過 v-directive="binding" 來帶入值
+    el.style.backgroundColor = binding.value;
+  }
+});
 
 
 new Vue({
